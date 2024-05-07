eb3f7b2d437aecbc2c5bfd8bdb44fff99dfd1999
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-05-07 09:52:16 -0300

    teste 2

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)  # Aplicando CORS para permitir acessos de qualquer origem
@@ -9,4 +10,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
