895bc4f3a16249a3253ea0fa737ac6639b0020e5
ESousa97 <xfuuuuuuuuury2018@gmail.com>
2025-07-16 00:10:35 -0300

    Bug: melhorar tratamento de dados em pt-br.

--- tests/test_analysis.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from analysis.statistical_analyzer import statistical_analyzer
-
-def test_basic_statistics():
-    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
-    stats = statistical_analyzer.analyze_dataset(df)
-    assert "basic_stats" in stats
--- tests/test_core.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from core.file_handler import FileHandler
-
-def test_load_from_bytes():
-    csv_content = b"id,nome\n1,Ana\n2,Bruno"
-    df = FileHandler.load_from_bytes(csv_content)
-    assert df.shape == (2, 2)
