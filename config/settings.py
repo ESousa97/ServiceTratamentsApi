fcab8a115d3af087a582636bd0c27da45b06bc55
ESousa97 <xfuuuuuuuuury2018@gmail.com>
2025-07-17 21:14:05 -0300

    Feat: Removidos openwordnet.

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 # config/settings.py
-MAX_ROWS = 10000
-MAX_FILE_SIZE_MB = 5
+MAX_ROWS = 10000000
+MAX_FILE_SIZE_MB = 1000
 DEFAULT_STOPWORDS = [
     "de", "a", "mas", "por", "e", "do", "da", "os", "as", "em", "um", "uma",
     "para", "com", "sem", "no", "na", "nos", "nas", "o", "à", "ao", "se", "que"
